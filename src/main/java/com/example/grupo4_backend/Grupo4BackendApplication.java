b44e52f492de73f2325ae4b358c6352fe6e197b2
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-06-27 18:19:29 -0400

    First Commit

---
+++ src/main/java/com/example/grupo4_backend/Grupo4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo4_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupo4_backend/Grupo4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo4_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

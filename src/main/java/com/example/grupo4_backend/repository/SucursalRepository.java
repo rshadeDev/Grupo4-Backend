e96cde232dc7474dbe43c0aec382116d006d9e60
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-06-29 17:58:18 -0400

    chore: Agregar interfaz de repositorio para la entidad Sucursal
    - Se ha creado SucursalRepository para manejar operaciones CRUD sobre la entidad Sucursal.
    - Extiende JpaRepository para aprovechar funcionalidades de Spring Data JPA.
    - Anotado con @Repository para indicar que es un componente de repositorio.

--- src/main/java/com/example/grupo4_backend/repository/SucursalRepository.java
+++ src/main/java/com/example/grupo4_backend/repository/SucursalRepository.java
@@ -1,5 +1,10 @@
 package com.example.grupo4_backend.repository;
 
-public class SucursalRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.grupo4_backend.model.Sucursal;
+
+@Repository
+public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
 }

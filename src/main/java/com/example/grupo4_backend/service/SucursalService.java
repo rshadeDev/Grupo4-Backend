63dc7b00b03530cc359c541a14e9b01ba84f7d03
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-06-29 18:01:32 -0400

    feat: Agregar servicio para operaciones CRUD de Sucursal
    - Se ha creado SucursalService para manejar operaciones de consulta, creación y eliminación de sucursales.
    - Inyecta SucursalRepository para interactuar con la capa de persistencia.
    - Métodos implementados: findAll(), findById(), save() y deleteById().

--- src/main/java/com/example/grupo4_backend/service/SucursalService.java
+++ src/main/java/com/example/grupo4_backend/service/SucursalService.java
@@ -1,5 +1,33 @@
 package com.example.grupo4_backend.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.grupo4_backend.model.Sucursal;
+import com.example.grupo4_backend.repository.SucursalRepository;
+
+@Service
 public class SucursalService {
     
+    @Autowired
+    private SucursalRepository sucursalRepository;
+
+    public List<Sucursal> findAll() {
+        return sucursalRepository.findAll();
+    }
+
+    public Optional<Sucursal> findById(Long id) {
+        return sucursalRepository.findById(id);
+    }
+
+    public Sucursal save(Sucursal sucursal) {
+        return sucursalRepository.save(sucursal);
+    }
+
+    public void deleteById(Long id) {
+        sucursalRepository.deleteById(id);
+    }
 }

37a314c393adf7fe1e26e6b6fe94acb719b4deda
Byersen <h.garrido02@ufromail.cl>
2024-07-02 00:56:23 -0400

    feat: añado CrossOrigin en UsuarioController

--- src/main/java/com/example/grupo4_backend/controller/UsuarioController.java
+++ src/main/java/com/example/grupo4_backend/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
